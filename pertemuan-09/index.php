986d4d991c71e597e584c42884d24ae9b29a54f4
indra <2522500068@mahasiswa.atmaluhur.ac.id>
2025-12-04 17:37:05 +0700

    first commit

--- pertemuan-09/index.php
+++ pertemuan-09/index.php
@@ -16,21 +16,55 @@ if (isset($_SESSION["sespesan"])):
   $sespesan = $_SESSION["sespesan"];
 endif;
 
-<?php
-$biodata = $_SESSION["biodata"] ?? [];
+$txtNim = "";
+if (isset($_SESSION["txtNim"])):
+  $txtNim = $_SESSION["txtNim"];
+endif;
 
-$fieldconfig = [
-  "nim" => ["label" => "nim", "suffix" => ""],
-  "nama" => ["label" => "nama lengkap", "suffix" => " &#128526;"],
-  "tempat" => ["label" => "Tempat Lahir", "suffix" => ""],
-  "tanggal" => ["label" => "Tanggal Lahir", "suffix" => ""],
-  "hobi" => ["label" => "Hobi", "suffix" => " &#127926;"],
-  "pasangan" => ["label" => "Pasangan", "suffix" => " &hearts;"],
-  "pekerjaan" => ["label" => "Pekerjaan", "suffix" => " &copy; 2025"],
-  "ortu" => ["label" => "Nama Orang Tua", "suffix" => ""],
-  "kakak" => ["label" => "Nama Kakak", "suffix" => ""],
-  "adik" => ["label" => "Nama Adik", "suffix" => ""],
-]
+$txtNmLengkap = "";
+if (isset($_SESSION["txtNmLengkap"])):
+  $txtNmLengkap = $_SESSION["txtNmLengkap"];
+endif;
+
+$txtT4Lhr = "";
+if (isset($_SESSION["txtT4Lhr"])):
+  $txtT4Lhr = $_SESSION["txtT4Lhr"];
+endif;
+
+$txtTglLhr = "";
+if (isset($_SESSION["txtTglLhr"])):
+  $txtTglLhr = $_SESSION["txtTglLhr"];
+endif;
+
+$txtHobi = "";
+if (isset($_SESSION["txtHobi"])):
+  $txtHobi = $_SESSION["txtHobi"];
+endif;
+
+$txtPasangan = "";
+if (isset($_SESSION["txtPasangan"])):
+  $txtPasangan = $_SESSION["txtPasangan"];
+endif;
+
+$txtKerja = "";
+if (isset($_SESSION["txtKerja"])):
+  $txtKerja = $_SESSION["txtKerja"];
+endif;
+
+$txtNmOrtu = "";
+if (isset($_SESSION["txtNmOrtu"])):
+  $txtNmOrtu = $_SESSION["txtNmOrtu"];
+endif;
+
+$txtNmKakak = "";
+if (isset($_SESSION["txtNmKakak"])):
+  $txtNmKakak = $_SESSION["txtNmKakak"];
+endif;
+
+$txtNmAdik = "";
+if (isset($_SESSION["txtNmAdik"])):
+  $txtNmAdik = $_SESSION["txtNmAdik"];
+endif;
 ?>
 
 <!DOCTYPE html>
@@ -120,17 +154,6 @@ $fieldconfig = [
 
     <section id="about">
       <h2>Tentang Saya</h2>
-      <?php foreach ($fieldconfig as $kunci => $metadata): ?>
-      <p>
-        <strong><?= $metadata["label"] ?></strong>
-        <?= htmlspecialchars(string: $biodata[$kunci] ?? "") ?>
-        <?= $metadata["suffix"] ?>
-      </p>
-      <?php endforeach; ?>
-      <?php
-echo "Hello"
-// missing closing tag;
-
       <p><strong>NIM:</strong> <?= $txtNim ?></p>
       <p><strong>Nama Lengkap:</strong> <?= $txtNmLengkap ?> &#128526;</p>
       <p><strong>Tempat Lahir:</strong> <?= $txtT4Lhr ?></p>
--- pertemuan-09/proses.php
+++ pertemuan-09/proses.php
@@ -7,26 +7,16 @@ $_SESSION["sesnama"] = $sesnama;
 $_SESSION["sesemail"] = $sesemail;
 $_SESSION["sespesan"] = $sespesan;
 
-foreach ($dataBiodata as $k => $v) {
-    echo "<p><strong>$k</strong>: $v</p>";
-}
-
-$_SESSION["biodata"] = $arrBiodata;
-header("location: index.php");
-
-$arrBiodata = [
-"nim" => $_POST["txtNim"] ?? "",
-"nama" => $_POST["txtNmLengkap"] ?? "",
-"tempat" => $_POST["txtT4Lhr"] ?? "",
-"tanggal" => $_POST["txtTglLhr"] ?? "",
-"hobi" => $_POST["txtHobi"] ?? "",
-"pasangan" => $_POST["txtPasangan"] ?? "",
-"pekerjaan" => $_POST["txtKerja"] ?? "",
-"ortu" => $_POST["txtNmOrtu"] ?? "",
-"kakak" => $_POST["txtNmKakak"] ?? "",
-"adik" => $_POST["txtNmAdik"] ?? ""
-];
-
+$txtNim = $_POST["txtNim"];
+$txtNmLengkap = $_POST["txtNmLengkap"];
+$txtT4Lhr = $_POST["txtT4Lhr"];
+$txtTglLhr = $_POST["txtTglLhr"];
+$txtHobi = $_POST["txtHobi"];
+$txtPasangan = $_POST["txtPasangan"];
+$txtKerja = $_POST["txtKerja"];
+$txtNmOrtu = $_POST["txtNmOrtu"];
+$txtNmKakak = $_POST["txtNmKakak"];
+$txtNmAdik = $_POST["txtNmAdik"];
 
 $_SESSION["txtNim"] = $txtNim;
 $_SESSION["txtNmLengkap"] = $txtNmLengkap;
